b8d68fe74f36af38b930b6fd29ebad0e5fc34a31
Manuel <darkpriestrelative@gmail.com>
2016-08-24 08:52:34 -0400

    REFACTOR: created InformationTab
    Also includes some UI tweaks

---
+++ stress-app/src/main/java/org/iop/stress_app/structure/controllers/tab/InformationTabController.java
@@ -0,0 +1,17 @@
+package org.iop.stress_app.structure.controllers.tab;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Manuel Perez P. (darkpriestrelative@gmail.com) on 24/08/16.
+ */
+public class InformationTabController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Not implemented right now
+    }
+}
