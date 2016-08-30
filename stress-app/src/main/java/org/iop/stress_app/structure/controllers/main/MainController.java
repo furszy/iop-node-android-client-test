5dd92e739984bd5d516a73e690c83b6328fdbb61
Manuel <darkpriestrelative@gmail.com>
2016-08-30 11:59:52 -0400

    REFACTOR: implement DynamicChoiceSelector.
    This class improves the ChoiceDialog class from JavaFx and adapts to Stress App.

--- stress-app/src/main/java/org/iop/stress_app/structure/controllers/main/MainController.java
+++ stress-app/src/main/java/org/iop/stress_app/structure/controllers/main/MainController.java
@@ -1,13 +1,22 @@
 package org.iop.stress_app.structure.controllers.main;
 
 import org.iop.stress_app.structure.controllers.AbstractMainController;
+import org.iop.stress_app.structure.views.DynamicChoiceSelector;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * This class is the Main view controller, please, add here all the logic to init the view
  * Created by Manuel Perez P. (darkpriestrelative@gmail.com) on 23/08/16.
  */
 public class MainController extends AbstractMainController {
-    /**
-     * This class only extends AbstractMainController, this can be changed in the future
-     */
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Showing a dialog to select an IP
+        DynamicChoiceSelector dynamicChoiceSelector = new DynamicChoiceSelector();
+        String nodeIP = dynamicChoiceSelector.showAndWaitResult();
+        System.out.println("I will work with the IP "+nodeIP);
+    }
 }
